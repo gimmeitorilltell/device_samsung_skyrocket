982563c340c5f43c3e2754e0a168224f4e1392ba
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-04-11 12:12:02 -0400

    make cm

---
+++ cm.mk
@@ -0,0 +1,21 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
+
+# Inherit some common cm stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := skyrocket
+
+# Device identifier
+PRODUCT_NAME := cm_skyrocket
+PRODUCT_DEVICE := skyrocket
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:5.0.2/LRX22G/I727UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 5.0.2 LRX22G I727UCMC1 release-keys"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo candy5_skyrocket-userdebug
+add_lunch_combo cm_skyrocket-eng
 
